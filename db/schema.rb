c5ce3428221caf850c3a25caa1fa3b15ad2c2e07
fedeagripa <fagripa@toptierlabs.com>
2016-11-02 17:36:25 -0300

    Add description + show details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102141103) do
+ActiveRecord::Schema.define(version: 20161102201534) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20161102141103) do
     t.string   "title"
     t.integer  "isbn"
     t.datetime "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "author_id"
+    t.string   "description"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
