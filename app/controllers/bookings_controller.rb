24c1c776235cbaf5fa941eb1aa32d1c288b8f156
fedeagripa <fagripa@toptierlabs.com>
2016-11-07 12:42:06 -0300

    Fix pr comments + migration to stat as enum

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,17 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :review_bookings, only: :index
   # GET /bookings
   # GET /bookings.json
   def index
-    @bookings = Booking.where('user_id' => current_user.id)
+    @bookings = Booking.where(user_id: current_user.id)
   end
 
   # POST /bookings
   # POST /bookings.json
   def create
     @booking = Booking.new(booking_params)
-    bookings = Booking.where(book_id: @booking.book.id,user_id: @booking.user.id)
-    if bookings != nil
-      @booking.stat = 'Denied'
-    else
-      @booking.stat = 'Pending'
-    end
     @booking.save
-
     redirect_to action: "index"
   end
 
@@ -32,15 +24,5 @@ class BookingsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
       params.permit(:book_id).merge(user_id: current_user.id)
-    end
-
-    def review_bookings
-      bookings = Booking.where(stat: "Aproved")
-      bookings.each do |b|
-        if b.created_at < 1.day.ago
-          b.stat = "Finished"
-          b.save
-        end
-      end
     end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,6 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
   # POST /comments
   # POST /comments.json
   def create
